54a9ca95c35b082143a0d93fa2ddfa123b97082a
NikoTie <78031894+Niko-Tiesmaki@users.noreply.github.com>
2025-01-16 14:21:33 +0200

    Update pat.js

--- ViteTest/src/pat.js
+++ ViteTest/src/pat.js
@@ -4,7 +4,6 @@ let count = 0
 export function patJeff (jeff) {
     jeff.addEventListener("click", ()=>{
         count++
-        console.log(count)
         jeff.innerHTML = "Pats " + count
     });
 };
