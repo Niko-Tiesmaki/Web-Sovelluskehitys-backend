1128f949a465a9f1ae4d11ff7927f30ade8ec435
NikoTie <nikojjt@gmail.com>
2025-01-17 11:24:56 +0200

    Task completed
    Completed to my understanding. Used Rest client to test responses.

--- week1-assingment/src/index.js
+++ week1-assingment/src/index.js
@@ -5,6 +5,61 @@ const port = 3000;
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.status(200);
+  res.setHeader('Content-type', 'text/plain')
+  res.send('Welcome to my rest api');
+});
+
+app.get('/moro', (req, res) => {
+  res.status(200);
+  res.setHeader('Content-type', 'text/plain')
+  res.send('Moro');
+});
+app.get('/moro/json', (req, res) => {
+  const moroJson = {
+    title: 'Moro',
+    description: 'Moro testin vastaus json formatissa',
+  };
+  res.status(200);
+  res.set('Content-type', 'application/json');
+  res.json(moroJson);
+});
+
+app.post('/moro', (req, res) => {
+  const data = {
+    Title: 'dummy data',
+    description: 'Test data for json response',
+  };
+  res.status(201);
+  res.set('Content-type', 'application/json');
+  res.json(data);
+});
+
+app.delete('/moro/:id', (req, res) => {
+  if (req.params.id === '99') {
+    res.status(204);
+    res.setHeader('Content-type', 'text/plain');
+    res.send('Content successfully deleted with id: ' + req.params.id);
+  } else {
+    res.status(404);
+    res.setHeader('Content-type', 'text/plain');
+    res.send('No content found with id: ' + req.params.id);
+  }
+});
+app.put('/moro/:id', (req, res) => {
+  if (req.params.id === '99') {
+    res.status(200);
+    res.json({
+      Title: 'Updated data',
+      description: 'Updated data from put request',
+    });
+  } else {
+    res.status(404);
+    res.send('No data found with id: ' + req.params.id);
+  }
+});
+
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
