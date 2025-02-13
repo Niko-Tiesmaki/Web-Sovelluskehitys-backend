b981bf0e8e149bbabe01df05725a389fea6935d2
NikoTie <nikojjt@gmail.com>
2025-02-13 11:50:25 +0200

    update

--- week5/src/controllers/auth-controller.js
+++ week5/src/controllers/auth-controller.js
@@ -1,19 +1,24 @@
-import { login } from "./user-controller.js";
+
 import { selectUserByNameAndPassword } from "../models/user-model.js";
 import jwt from 'jsonwebtoken';
 import 'dotenv/config';
 
 
 const postLogin = async (req, res) => {
-    console.log('postLogin', req.body);
-    const user = await login(req.body);
-    if (user) {
-        const token = jwt.sign(user, process.env.JWT_SECRET, {expiresIn: '24h'});
-        return token;
-    }else {
-        res.sendStatus(401);
+    const {username, password} = req.body;
+    if (!username) {
+      return res.status(401).json({message: 'Username missing.'});
     }
-};
+    const user = await selectUserByNameAndPassword(username, password);
+    if (user) {
+      const token = jwt.sign(user, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+      });
+      res.json({message: 'login ok', user, token});
+    } else {
+      res.status(401).json({message: 'Bad username/password.'});
+    }
+  };
 
 const getMe = async (req, res) =>{
     console.log('getMe', req.user);
